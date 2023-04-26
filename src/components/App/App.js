7dc40f60a102f7ae25d69c2876dbea1282421420
techsavvysos <kedogosospeter36@gmail.com>
2023-04-26 22:49:07 +0100

    done with all the CSS

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,6 +5,29 @@ function App() {
     <div className="App">
       <div className='calculator'>
         <div className='display' id="display"></div>
+        <div className='input'>
+          <button id="clear" className='clear'>AC</button>
+          <button id="divide" className='divide'>/</button>
+          <button id="multiply" className='multiply'>x</button>
+          <button id="equals" className='equals'>=</button>
+          <button id="nine" className='nine'>9</button>
+          <button id="eight" className='eight'>8</button>
+          <button id="seven" className='seven'>7</button>
+          <button id="six" className='six'>6</button>
+          <button id="five" className='five'>5</button>
+          <button id="four" className='four'>4</button>
+          <button id="three" className='three'>3</button>
+          <button id="two" className='two'>2</button>
+          <button id="one" className='one'>1</button>
+          <button id="zero" className='zero'>0</button>
+
+          <button id="add" className='add'>+</button>
+          <button id="subtract" className='subtract'>-</button>
+          
+          
+          <button id="decimal" className='decimal'>.</button>
+          
+        </div>
       </div>
       <p>Designed and Coded by <br/> TechSavvySos</p>
     </div>
