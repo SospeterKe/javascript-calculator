773ea96d730e0eb83ec7504f7572531716a62147
techsavvysos <kedogosospeter36@gmail.com>
2023-04-26 21:50:49 +0100

    created javascript app

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='calculator'>
+        <div className='display' id="display"></div>
+      </div>
+      <p>Designed and Coded by <br/> TechSavvySos</p>
+    </div>
+  );
+}
+
+export default App;
