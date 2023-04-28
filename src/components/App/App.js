824aca762f0addaf8dc7bb5cc5347e8c227fb745
techsavvysos <kedogosospeter36@gmail.com>
2023-04-28 15:36:04 +0100

    used eval javascript function for calculations

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,64 +1,67 @@
 import { useState } from 'react';
 import './App.css';
 
-function App() {
+export default function App() {
 
-  const [clickedNumbers, setClickedNumber] = useState([0]);
-  const [currentOperation, setCurrentOperation] = useState({
-    operator: null,
-    operand: null
-  });
+  const [clickedNumbers, setClickedNumber] = useState(['0']);
+  const [result, setResult] = useState('0');
   const clearButton = 'AC';
 
-
   function handleClick(number) {
-    if(clickedNumbers[0] === 0 || clickedNumbers[0] === 'AC'){
-      setClickedNumber([number]);
+    let newClickedNumbers = [];
+
+
+    if (number === "." && clickedNumbers[0] === 0 ) {
+      newClickedNumbers = ['0', '.'];
+    } else if (clickedNumbers[0] === 0) {
+      newClickedNumbers = [number];
     } else {
-        if(number === clearButton)
-          setClickedNumber([0]);
-        else 
-          setClickedNumber([...clickedNumbers, number]);
+      newClickedNumbers = [...clickedNumbers, number];
+    }
+  
+    if (number === clearButton) {
+      newClickedNumbers = [0];
+      setResult('0');
     }
 
-    switch(number){
-      case '+':
-        setCurrentOperation({
-          operator: '+',
-          operand: Number(clickedNumbers.join(''))
-        });
-      break;
-
-      default: 
-        setCurrentOperation({
-          operator: '#',
-          operand: Number(clickedNumbers.join(''))
-        });
-    }
+    setClickedNumber(newClickedNumbers);
   }
+
+
+  const handleEqualClick = () => {
+    let expression = clickedNumbers.join('');
+    function parse(str) {
+      // eslint-disable-next-line no-new-func
+      return Function(`'use strict'; return (${str})`)()
+  }
+    let finalResult = parse(expression);
+    setResult(finalResult);
+  }
+
+  
 
   return (
     <div className="App">
       <div className='calculator'>
         <div className='display' id="display">
-          <div className='mainExpression' id="mainExpression">{clickedNumbers.join('')}</div>
+          <div className='mainExpression' id="mainExpression">{result}</div>
           <div className='currentInput' id="currentInput">{clickedNumbers}</div>
         </div>
         <div className='input'>
           <button id="clear" className='clear' onClick={() => handleClick('AC')}>AC</button>
           <button id="divide" className='divide' onClick={() => handleClick("/")}>/</button>
-          <button id="multiply" className='multiply' onClick={() => handleClick("x")}>x</button>
-          <button id="equals" className='equals' onClick={() => handleClick("=")}>=</button>
-          <button id="nine" className='nine' onClick={() => handleClick(9)}>9</button>
-          <button id="eight" className='eight' onClick={() => handleClick(8)}>8</button>
-          <button id="seven" className='seven' onClick={() => handleClick(7)}>7</button>
-          <button id="six" className='six' onClick={() => handleClick(6)}>6</button>
-          <button id="five" className='five' onClick={() => handleClick(5)}>5</button>
-          <button id="four" className='four' onClick={() => handleClick(4)}>4</button>
-          <button id="three" className='three' onClick={() => handleClick(3)}>3</button>
-          <button id="two" className='two' onClick={() => handleClick(2)}>2</button>
-          <button id="one" className='one' onClick={() => handleClick(1)}>1</button>
-          <button id="zero" className='zero' onClick={() => handleClick(0)}>0</button>
+          <button id="multiply" className='multiply' onClick={() => handleClick("*")}>x</button>
+          <button id="equals" className='equals' onClick={() => handleEqualClick()}>=</button>
+          <button id="nine" className='nine' onClick={() => handleClick('9')}>9</button>
+          <button id="eight" className='eight' onClick={() => handleClick('8')}>8</button>
+          <button id="seven" className='seven' onClick={() => handleClick('7')}>7</button>
+          <button id="six" className='six' onClick={() => handleClick('6')}>6</button>
+          <button id="five" className='five' onClick={() => handleClick('5')}>5</button>
+          <button id="four" className='four' onClick={() => handleClick('4')}>4</button>
+          <button id="three" className='three' onClick={() => handleClick('3')}>3</button>
+          <button id="two" className='two' onClick={() => handleClick('2')}>2</button>
+          <button id="one" className='one' onClick={() => handleClick('1')}>1</button>
+          <button id="zero" className='zero' onClick={() => handleClick('0')}>0</button>
           <button id="add" className='add' onClick={() => handleClick("+")}>+</button>
           <button id="subtract" className='subtract' onClick={() => handleClick("-")}>-</button>
           <button id="decimal" className='decimal' onClick={() => handleClick(".")}>.</button>
@@ -68,5 +71,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
